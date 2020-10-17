c5908e17d6b0cbfa5449d7df66c95f2cb7b9d512
hossein nick <hossinnick62@gmail.com>
2020-10-17 18:53:04 +0330

    create mode classes and annotated them

---
+++ src/main/java/home/train/model/Professor.java
@@ -0,0 +1,27 @@
+package home.train.model;
+
+import javax.persistence.Entity;
+
+@Entity
+public class Professor extends Person{
+
+    private String speciality;
+
+    public Professor() {
+    }
+
+    public String getSpeciality() {
+        return speciality;
+    }
+
+    public void setSpeciality(String speciality) {
+        this.speciality = speciality;
+    }
+
+    @Override
+    public String toString() {
+        return "Professor{" + super.toString()+
+                "speciality='" + speciality + '\'' +
+                '}';
+    }
+}
---
+++ src/main/java/home/train/model/Student.java
@@ -0,0 +1,27 @@
+package home.train.model;
+
+import javax.persistence.Entity;
+
+@Entity
+public class Student extends Person {
+
+    private String course;
+
+    public Student() {
+    }
+
+    public String getCourse() {
+        return course;
+    }
+
+    public void setCourse(String course) {
+        this.course = course;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" + super.toString()+
+                "course='" + course + '\'' +
+                '}';
+    }
+}
