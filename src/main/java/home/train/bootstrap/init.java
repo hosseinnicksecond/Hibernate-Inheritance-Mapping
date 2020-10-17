92c585dcf4685a7af63c5001087072353ff37b94
hossein nick <hossinnick62@gmail.com>
2020-10-17 20:55:54 +0330

    create PersonRepository nd init.java

---
+++ src/main/java/home/train/bootstrap/init.java
@@ -0,0 +1,35 @@
+package home.train.bootstrap;
+
+import home.train.model.Professor;
+import home.train.model.Student;
+import home.train.repository.PersonRepository;
+import org.springframework.boot.CommandLineRunner;
+
+public class init implements CommandLineRunner {
+
+    private final PersonRepository repository;
+
+    public init(PersonRepository repository) {
+        this.repository = repository;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        Student student = new Student();
+        student.setFirstName("john");
+        student.setLastName("Doe");
+        student.setEmail("doe@email.com");
+        student.setCourse("java");
+
+        Professor professor = new Professor();
+        professor.setFirstName("mary");
+        professor.setLastName("public");
+        professor.setEmail("mary@email.com");
+        professor.setSpeciality("programming");
+
+        repository.save(student);
+        repository.save(professor);
+
+        repository.findAll().forEach(s-> System.out.println(s));
+    }
+}
